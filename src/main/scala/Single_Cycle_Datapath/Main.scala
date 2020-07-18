6eb29ee653d082b9b2c079c67084314c24956b99
a163236 <51395204+a163236@users.noreply.github.com>
2020-07-18 11:59:37 +0900

    不要ファイル削除

--- src/main/scala/Single_Cycle_Datapath/Main.scala
+++ src/main/scala/Single_Cycle_Datapath/Main.scala
@@ -5,13 +5,14 @@ import java.nio.file.{Files, Paths}
 
 
 object Main extends App {
-  val dir = new File(args(0)) // args(0)のディレクトリのオブジェクトを作って
-  dir.mkdirs                  // args(0)ディレクトリを作成する
+  //val dir = new File(args(0)) // args(0)のディレクトリのオブジェクトを作って
+  //dir.mkdirs                  // args(0)ディレクトリを作成する
 
   val verilogString = (new chisel3.stage.ChiselStage).emitVerilog(new dpath)
-  val verilog = new FileWriter(new File(dir, s"main.v"))
-  verilog write verilogString
-  verilog.close()
+
+  //val verilog = new FileWriter(new File(dir, s"main.v"))
+  //verilog write verilogString
+  //verilog.close()
 
   println(verilogString) // verilog のprint
 
